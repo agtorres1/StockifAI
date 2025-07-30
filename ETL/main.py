5956325401989e99e5c4dea821e6c2cc66c917a8
agustina <agtorres@frba.utn.edu.ar>
2025-07-30 18:10:09 -0300

    configuraciones

---
+++ ETL/main.py
@@ -0,0 +1,8 @@
+from app.mi_clase import MiClase  # ajustalo a tu estructura
+
+def main():
+    instancia = MiClase()
+    instancia.ejecutar()  # o lo que haga tu clase
+
+if __name__ == "__main__":
+    main()
