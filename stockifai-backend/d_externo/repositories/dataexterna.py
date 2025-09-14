7fe83182e181f4389dfd56c028a9776cc60433de
GonzaSchaller <gschaller@frba.utn.edu.ar>
2025-09-14 04:09:33 -0300

    Modelo Integrado con la base

--- stockifai-backend/d_externo/repositories/dataexterna.py
+++ stockifai-backend/d_externo/repositories/dataexterna.py
@@ -40,19 +40,36 @@ def obtener_registroentrenamiento_intermitente(taller_id: int):
     lista_diccionarios = list(registros.values())
     return lista_diccionarios
 
+def borrar_registroentrenamiento_intermitente(taller: Taller):
+    """
+    Borra todos los registros de la tabla RegistroEntrenamiento_intermitente
+    asociados a un taller específico.
+    """
+    try:
+        registros_borrados, _ = RegistroEntrenamiento_intermitente.objects.filter(taller=taller).delete()
+        print(f"Se eliminaron {registros_borrados} registros de Intermitente del taller {taller.nombre}.")
+    except Exception as e:
+        print(f"Error al borrar registros de Intermitente: {e}")
+        raise
 
 def guardar_registroentrenamiento_intermitente(datos: dict, taller: Taller):
     """
     Guarda un registro en la base de datos para RegistroEntrenamiento_intermitente.
     """
+    numero_pieza = datos.pop('numero_pieza')
     try:
-        registro = RegistroEntrenamiento_intermitente.objects.create(
-            taller=taller,
-            **datos
+        registro = RegistroEntrenamiento_intermitente.objects.update_or_create(
+            numero_pieza=numero_pieza,
+            defaults={
+                **datos, # Todos los demás datos del diccionario
+                'taller': taller,
+                'segmento_demanda': 'intermitente'
+            }
         )
         return registro
-    except Taller.DoesNotExist:
-        raise ValueError(f"No se encontró un Taller con id={taller_id}")
+    except Exception as e:
+        print(f"Error al crear registro de Intermitente: {e}")
+        raise
 
 
 def obtener_registroentrenamiento_frecuencia_alta(taller_id: int):
@@ -64,16 +81,32 @@ def obtener_registroentrenamiento_frecuencia_alta(taller_id: int):
     lista_diccionarios = list(registros.values())
     return lista_diccionarios
 
-
+def borrar_registroentrenamiento_frecuencia_alta(taller: Taller):
+    """
+    Borra todos los registros de la tabla RegistroEntrenamiento_intermitente
+    asociados a un taller específico.
+    """
+    try:
+        registros_borrados, _ = RegistroEntrenamiento_Frecuencia_Alta.objects.filter(taller=taller).delete()
+        print(f"Se eliminaron {registros_borrados} registros de Frecuencia Alta del taller {taller.nombre}.")
+    except Exception as e:
+        print(f"Error al borrar registros de Intermitente: {e}")
+        raise
 def guardar_registroentrenamiento_frecuencia_alta(datos: dict, taller: Taller):
     """
     Guarda un registro en la base de datos para RegistroEntrenamientoFrecuenciaAlta.
     """
+    numero_pieza = datos.pop('numero_pieza')
     try:
-        registro = RegistroEntrenamiento_Frecuencia_Alta.objects.create(
-            taller=taller,
-            **datos
+        registro = RegistroEntrenamiento_Frecuencia_Alta.objects.update_or_create(
+            numero_pieza=numero_pieza,
+            defaults={
+                **datos, # Todos los demás datos del diccionario
+                'taller': taller,
+                'segmento_demanda': 'frecuencia_alta'
+            }
         )
         return registro
-    except Taller.DoesNotExist:
-        raise ValueError(f"No se encontró un Taller con id={taller_id}")
+    except Exception as e:
+        print(f"Error al crear registro de Frecuencia Alta: {e}")
+        raise
