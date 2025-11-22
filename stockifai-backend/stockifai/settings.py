22a93e3e801691628a9b513606be26ebcf17dbbd
Gonzalo Garcia <gonzalogarcia2318@gmail.com>
2025-11-22 20:28:20 -0300

    Fix settings.

--- stockifai-backend/stockifai/settings.py
+++ stockifai-backend/stockifai/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware"
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware","django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware","django.contrib.messages.middleware.MessageMiddleware",
