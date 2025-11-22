5b90609ca0cb0bb50d30403dba1bb416f2ed2888
Gonzalo Garcia <gonzalogarcia2318@gmail.com>
2025-11-22 20:18:41 -0300

    Fix 2 cors.

--- stockifai-backend/stockifai/settings.py
+++ stockifai-backend/stockifai/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware"
     "django.middleware.security.SecurityMiddleware","django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware","django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware","django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
    # 'user.middleware.Auth0Middleware',
@@ -164,6 +165,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_HEADERS = True
 CORS_ALLOW_ALL_METHODS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 SESSION_COOKIE_HTTPONLY = True   # Seguridad: no accesible desde JavaScript
